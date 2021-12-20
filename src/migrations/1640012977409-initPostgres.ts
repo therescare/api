e828bd01e513bce60f4348bc5133bca4937bec0a
Lewis Crichton <lewistehminerz@outlook.com>
2021-12-20 15:12:17 +0000

    fix migration

--- src/migrations/1640012977409-initPostgres.ts
+++ src/migrations/1640012977409-initPostgres.ts
@@ -4,6 +4,7 @@ export class initPostgres1640012977409 implements MigrationInterface {
 	name = 'initPostgres1640012977409';
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
 		await queryRunner.query(
 			`CREATE TABLE "pending_verification" ("id" character varying NOT NULL, "email" character varying NOT NULL, CONSTRAINT "UQ_c7f6f11ee662e6674c48c1d8cae" UNIQUE ("email"), CONSTRAINT "PK_09c0bd8ad0770d9e05f5e5541fd" PRIMARY KEY ("id"))`
 		);
